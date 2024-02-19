bf0213dc1e367ca0938d8bae71bc49b9f3b175d1
Yixiao <1411101569@qq.com>
2024-02-19 13:38:25 +0000

    add Line JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,20 @@
-import React, { Suspense, useState } from 'react'
-import { Interactive, XR, ARButton, Controllers } from '@react-three/xr'
-import { Text } from '@react-three/drei'
+import React, {Suspense, useState} from 'react'
+import {Interactive, XR, ARButton, Controllers} from '@react-three/xr'
 import './App.css'
-import { Canvas } from '@react-three/fiber'
+import {Canvas} from '@react-three/fiber'
+import Scene from "./scene";
 
-function Box({ color, size, scale, children, ...rest }: any) {
-  return (
-    <mesh scale={scale} {...rest}>
-      <boxGeometry args={size} />
-      <meshPhongMaterial color={color} />
-      {children}
-    </mesh>
-  )
-}
-
-function Button(props: any) {
-  const [hover, setHover] = useState(false)
-  const [color, setColor] = useState<any>('blue')
-
-  const onSelect = () => {
-    setColor((Math.random() * 0xffffff) | 0)
-  }
-
-  return (
-    <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
-      <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.6, 0.6, 0.6]} size={[0.4, 0.1, 0.1]} {...props}>
-        <Suspense fallback={null}>
-          <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
-            Hello react-xr!
-          </Text>
-        </Suspense>
-      </Box>
-    </Interactive>
-  )
-}
 
 export default function App() {
-  return (
-    <>
-      <ARButton />
-      <Canvas>
-        <XR referenceSpace="local">
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Button position={[0, 0, -1]} />
-          <Controllers />
-        </XR>
-      </Canvas>
-    </>
-  )
+    return (
+        <>
+            <ARButton/>
+            <Canvas>
+                <XR referenceSpace="local">
+                    <Scene></Scene>
+                    <Controllers />
+                </XR>
+            </Canvas>
+        </>
+    )
 }
---
+++ src/scene.tsx
@@ -0,0 +1,64 @@
+import React, {Suspense, useEffect, useLayoutEffect, useRef, useState} from "react";
+import {Interactive} from "@react-three/xr";
+import {Line, Text} from "@react-three/drei";
+import {useFrame, useThree} from "@react-three/fiber";
+
+function Box({color, size, scale, children, ...rest}: any) {
+    return (
+        <mesh scale={scale} {...rest}>
+            <boxGeometry args={size}/>
+            <meshPhongMaterial color={color}/>
+            {children}
+        </mesh>
+    )
+}
+
+function Line2Button() {
+    const {camera} = useThree();
+    const [points, setPoints] = useState<Array<number>>([0, -0.7, 0, 0, -0.7, -1])
+
+    // Update line position every frame
+    useFrame(() => {
+        if (camera.position) {
+            const cameraPosition = camera.position;
+            setPoints([cameraPosition.x, -0.7, cameraPosition.z, 0, -0.7, -1]);
+        }
+    });
+
+    return (
+        <Line points={points} linewidth={10} color={"red"}/>
+    );
+}
+
+function Button(props: any) {
+    const [hover, setHover] = useState(false)
+    const [color, setColor] = useState<any>('blue')
+
+    const onSelect = () => {
+        setColor((Math.random() * 0xffffff) | 0)
+    }
+
+    return (
+        <Interactive onHover={() => setHover(true)} onBlur={() => setHover(false)} onSelect={onSelect}>
+            <Box color={color} scale={hover ? [0.6, 0.6, 0.6] : [0.6, 0.6, 0.6]} size={[0.4, 0.1, 0.1]} {...props}>
+                <Suspense fallback={null}>
+                    <Text position={[0, 0, 0.06]} fontSize={0.05} color="#000" anchorX="center" anchorY="middle">
+                        Hello react-xr!
+                    </Text>
+                </Suspense>
+            </Box>
+        </Interactive>
+    )
+}
+
+
+export default function Scene() {
+    return (
+        <>
+            <ambientLight/>
+            <pointLight position={[10, 10, 10]}/>
+            <Button position={[0, 0, -1]}/>
+            <Line2Button/>
+        </>
+    )
+}
