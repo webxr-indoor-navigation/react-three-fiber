28b3542b7e745fe1f5111dde750023f02c47e49d
Yixiao <1411101569@qq.com>
2024-02-21 22:48:02 +0000

    add startPOI initialization

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Navigator from "./navigator";
 export default function App() {
     return (
         <>
-            <Navigator poiJsonPath={process.env.PUBLIC_URL+'/test/poi.json'}/>
+            <Navigator poiJsonPath={process.env.PUBLIC_URL+'/test/poi.json'}  startUUID={'1249b4ad-d873-46d2-8699-a4c1a45dfc2f'}/>
         </>
     )
 }
--- src/navigator.tsx
+++ src/navigator.tsx
@@ -5,19 +5,36 @@ import React, {useEffect, useState} from "react";
 import {PoiItem} from "./types";
 import POIsWindow from "./POIsWindow";
 
-export default function Navigator({poiJsonPath}: any) {
+interface NavigatorProps {
+    poiJsonPath: string;
+    startUUID:  string;
+}
+
+export default function Navigator(props: NavigatorProps) {
     const [endPOI, setEndPOI] = useState<PoiItem>();
     const [startPOI, setStartPOI] = useState<PoiItem>();
     const [options, setOptions] = useState<PoiItem[]>([] as PoiItem[]); // 使用类型断言来指定初始类型
 
     useEffect(() => {
-        fetch(poiJsonPath)
+        fetch(props.poiJsonPath)
             .then(response => response.json())
             .then((data: PoiItem[]) => {
                 setOptions(data);
             })
             .catch(error => console.error('Error fetching data:', error));
-    }, [poiJsonPath]);
+
+
+    }, [props.poiJsonPath]);
+
+    useEffect(() => {
+        const selectedPOI = options.find(poi => poi.uuid === props.startUUID);
+        if (selectedPOI){
+            setStartPOI(selectedPOI);
+            console.log("find the startUUID: "+selectedPOI.name);
+        }else {
+            console.log("not find the startUUID: "+props.startUUID);
+        }
+    }, [props.startUUID, options]);
 
     const handleDropdownChange = (selectedUuid: string) => {
         const selectedPOI = options.find(poi => poi.uuid === selectedUuid);
