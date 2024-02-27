91ebb642248d6dfca3493aa96f5e60599ce0f2c7
Yixiao <1411101569@qq.com>
2024-02-27 18:29:25 +0000

    ops: remove privacyControl for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,15 @@ export default function App() {
 
     return (
         <>
-            {showAR ? (
+            {/*{showAR ? (*/}
                 <ARButton/>
-            ) : null
-            }
-            <CookieConsent
-                location={'bottom'}
-                onAccept={handleCookieConsentAccepted}
-                cookieName={'IndoorNavigatorPrivacyCookie'}
-                overlay>Your location is not being tracked.</CookieConsent>
+            {/*) : null*/}
+            {/*}*/}
+            {/*<CookieConsent*/}
+            {/*    location={'bottom'}*/}
+            {/*    onAccept={handleCookieConsentAccepted}*/}
+            {/*    cookieName={'IndoorNavigatorPrivacyCookie'}*/}
+            {/*    overlay>Your location is not being tracked.</CookieConsent>*/}
             <Navigator poiJsonPath={process.env.PUBLIC_URL + '/test/poi.json'}
                        startUUID={'12beebf7-6a6d-4c33-ba9f-3d96887a48cf'}/>
 
